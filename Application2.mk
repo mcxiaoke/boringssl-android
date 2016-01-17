aa21e8b70a3d65e09c5748deacb176e5a32b7ab3
mcxiaoke <mcxiaoke@gmail.com>
2016-01-17 19:42:30 +0800

    fix ndk build script, add .gitignore, add build.sh

---
+++ Application2.mk
@@ -0,0 +1,7 @@
+#APP_ABI := all
+#APP_ABI := armeabi armeabi-v7a arm64_v8a x86 x86_64 mips mips64
+APP_ABI := armeabi x86
+APP_STL := gnustl_static
+APP_PLATFORM := android-15
+APP_BUILD_SCRIPT := Android2.mk
+APP_CFLAGS := -DOPENSSL_NO_ASM
